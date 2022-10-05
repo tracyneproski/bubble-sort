7fcb1919ef947c83b903404c7ee6e60ba7e7d6a1
tracyneproski <tracyneproski@gmail.com>
2022-10-04 21:16:56 -0500

    Add very first steps to bubble sort skeleton

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,3 +3,12 @@
 # Build a method #bubble_sort that takes an array 
 # and returns a sorted array. It must use the bubble sort methodology.
 
+def bubble_sort(unsorted_array)
+
+
+# Check first and second number, if first is greater than second, switch.
+# Check second and third number, if second is greater than third, switch.
+# etc
+# When the end of the array has been reached, either end or start over depending
+# on whether the array is sorted properly or not.
+
