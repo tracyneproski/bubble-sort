a77b86cf57aa5a163985d9ff5f991ce5bed03473
tracyneproski <tracyneproski@gmail.com>
2022-10-07 13:16:52 -0500

    Add initial sorting attempt

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,8 +2,27 @@
 
 # Build a method #bubble_sort that takes an array 
 # and returns a sorted array. It must use the bubble sort methodology.
+# unsorted_array = [15,2,4,12,8]
+
+#index = 0
+#pair = [unsorted_array[index], unsorted_array[index + 1]]
+#unsorted_array.each_with_index do |number, index|
 
 def bubble_sort(unsorted_array)
+  index = 0
+  pair = [unsorted_array[index], unsorted_array[index + 1]]
+
+  if unsorted_array[index] > unsorted_array[index+1]
+    pair = [unsorted_array[index + 1], unsorted_array[index]]
+    unsorted_array.shift(pair[1])
+  else
+    index += 1
+  end
+
+
+  end
+end
+
 
 
 # Check first and second number, if first is greater than second, switch.
@@ -11,4 +30,6 @@ def bubble_sort(unsorted_array)
 # etc
 # When the end of the array has been reached, either end or start over depending
 # on whether the array is sorted properly or not.
+
+
 
